791f90ab244c02bc77d978d42b6c2797e534b4a0
GregoryPostDavis <gregoryp500@gmail.com>
2023-08-10 19:23:07 -0500

    it works :) capitalization issue

--- seasonAnalyzer.py
+++ seasonAnalyzer.py
@@ -20,17 +20,14 @@ class Pokemon:
         for teras in self.teraTypes:
             print("   ", teras)
 
-    def usedPokemon(self):
-        self.timesBrought = self.timesBrought + 1
-
     def add_unique_move(self, new_move):
         if not any(move1 == new_move for move1 in self.moveList):
             self.moveList.append(new_move)
-            print("added", new_move, "for", self.name)
-            for x in self.moveList:
-                print(x)
+            #print("added", new_move, "for", self.name)
+            # for x in self.moveList:
+            #     print(x)
         else:
-            print("did not add", new_move, "for", self.name)
+            #print("did not add", new_move, "for", self.name)
             pass
 
     def add_unique_tera(self, new_tera):
@@ -45,15 +42,12 @@ class Trainer:
         self.name = mon_name
         self.pokemon = []
 
-    def printName(self):
-        print(self.name)
-
     def add_unique_pokemon(self, new_pokemon):
         if not any(mon1.name == new_pokemon for mon1 in self.pokemon):
             self.pokemon.append(Pokemon(new_pokemon))
-            print(new_pokemon, "added")
+            #print(new_pokemon, "added")
         else:
-            print(new_pokemon, "not added")
+            #print(new_pokemon, "not added")
             pass
 
     def writeLog(self):
@@ -73,6 +67,10 @@ class Trainer:
         g.close()
 
 
+nicknames = dict()
+Trainers = []
+
+
 def nick_to_name(nick, nicknamed):
     for poke in nicknamed.keys():
         if nicknamed[poke] == nick:
@@ -81,44 +79,38 @@ def nick_to_name(nick, nicknamed):
             return poke
 
 
-nicknames = dict()
-Trainers = []
-
-
 def add_unique_trainer(new_trainer):
-    if not any(trainer1.name == new_trainer for trainer1 in Trainers):
+    if not any(trainer.name == new_trainer.lower() for trainer in Trainers):
         if len(new_trainer.strip()) > 0:
-            Trainers.append(Trainer(new_trainer))
-            #print("new trainer added")
+            Trainers.append(Trainer(new_trainer.lower()))
     else:
-        #print("no trainer added")
         pass
 
 
+#Establish File Path as Current Directory and take all html files
 mypath = getcwd()
 onlyFiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f.endswith(".html")]
-
-
-# for entry in onlyFiles:
-#     print(entry)
-# print("="*10)
+#
 
 
 for entry in onlyFiles:
     with open(entry, encoding='utf-8') as f:
         file = f.read()
     print("="*10, entry, "="*10)
+    for t in Trainers:
+        print (t.name)
+    # for c in Trainers:
+    #     for mons in c.pokemon:
+    #         mons.printPokemon()
+    #     print("="*10)
 
     file = file.split("<script type=\"text/plain\" class=\"battle-log-data\">")[1]
     file = file.split("</script>")[0]
     lines = file.split("\n")
 
-    players = dict()
     usernames = []
+    players = dict()
     nicks = dict()
-    last_move = []
-    last_source = ""
-    turn = 0
     currently_out = dict()
 
     #Acutal Looping goes in here
@@ -133,25 +125,21 @@ for entry in onlyFiles:
         if prefix == "player":
             players[args[0]] = args[1]
             usernames.append(args[1])
-            add_unique_trainer(args[1])
+            add_unique_trainer(args[1].lower())
 
         elif prefix == "poke":
             postCut = args[1].split(",")
             pkmn = postCut[0]
-            #print(postCut[0])
 
             for trainer in Trainers:
-                if trainer.name == players.get(args[0]):
-                    #print(players.get(args[0]))
+                if trainer.name.lower() == players.get(args[0]).lower():
                     trainer.add_unique_pokemon(pkmn)
                     for mon in trainer.pokemon:
                         if mon.name == pkmn:
-                            print(mon.name, mon.timesBrought)
+                            #print(mon.name, mon.timesBrought)
                             mon.timesBrought = mon.timesBrought + 1
                         pass
-                # for p in x.pokemon:
-                #     print(p.name)
-                # print("="*20)
+            pass
 
         elif prefix == "switch" or prefix == "drag":
 
@@ -161,33 +149,28 @@ for entry in onlyFiles:
             #(currently_out.get(player_id))
 
         elif prefix == "move":
-            target = args[2]
-            move = args[1]
             user = args[0]
-
+            move = args[1]
             if nick_to_name(user, nicks):
                 user = nick_to_name(user, nicks)
-            if nick_to_name(target, nicks):
-                target = nick_to_name(target, nicks)
-
-            #print("*-"*30, user)
 
             if args[0].startswith("p1"):
-                attacker = players.get("p1")
-                print(players.get("p1"))
+                attacker = players.get("p1").lower()
+                #print(attacker)
                 for trainer in Trainers:
                     if trainer.name == attacker:
                         for mons in trainer.pokemon:
                             if mons.name == user:
                                 mons.add_unique_move(move)
             else:
-                attacker = players.get("p2")
-                print(players.get("p2"))
+                attacker = players.get("p2").lower()
+                #print(attacker)
                 for trainer in Trainers:
                     if trainer.name == attacker:
                         for mons in trainer.pokemon:
                             if mons.name == user:
                                 mons.add_unique_move(move)
+            pass
 
         elif prefix == "-terastallize":
             tera_type = args[1]
@@ -197,19 +180,20 @@ for entry in onlyFiles:
                 user = nick_to_name(user, nicks)
 
             if args[0].startswith("p1"):
-                attacker = players.get("p1")
+                attacker = players.get("p1").lower()
                 for trainer in Trainers:
                     if trainer.name == attacker:
                         for mons in trainer.pokemon:
                             if mons.name == user:
                                 mons.add_unique_tera(tera_type)
             else:
-                attacker = players.get("p2")
+                attacker = players.get("p2").lower()
                 for trainer in Trainers:
                     if trainer.name == attacker:
                         for mons in trainer.pokemon:
                             if mons.name == user:
                                 mons.add_unique_tera(tera_type)
+            pass
 
     ##############
     #End of File #
